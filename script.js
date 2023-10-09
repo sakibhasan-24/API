b32c2aeeaccb3dca4fa8a194a36d3ecc4b3015a6
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-10-10 00:52:41 +0600

    User Show

--- script.js
+++ script.js
@@ -5,11 +5,40 @@ const course = {
   nameOFFaculty: ["a", "m", "Z"],
   isPrerequisite: true,
 };
-console.log(typeof course);
-console.log(course);
-const courseStringify = JSON.stringify(course);
-console.log(typeof courseStringify);
-console.log(courseStringify);
-const parseString = JSON.parse(courseStringify);
-console.log(typeof parseString);
-console.log(parseString);
+// console.log(typeof course);
+// console.log(course);
+// const courseStringify = JSON.stringify(course);
+// console.log(typeof courseStringify);
+// console.log(courseStringify);
+// const parseString = JSON.parse(courseStringify);
+// console.log(typeof parseString);
+// console.log(parseString);
+
+function calling() {
+  document.getElementById("calling").style.opacity = 0;
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => res.json())
+    .then((data) => display(data));
+  function display(users) {
+    const personContainer = document.getElementById("persons");
+    //   console.log(personContainer);
+    for (const user of users) {
+      const div = document.createElement("div");
+
+      personContainer.style.opacity = 1;
+      div.innerHTML = `
+    <div class="container">
+    <h1 class="title">${user.name}</h1>
+    <div class="info">
+      <h3>${user.email}</h3>
+      <h3>${user.address}</h3>
+      <p>${user.company}</p>
+      <h3>${user.website}</h3>
+    </div>
+    `;
+
+      personContainer.appendChild(div);
+      // console.log(personContainer);
+    }
+  }
+}
